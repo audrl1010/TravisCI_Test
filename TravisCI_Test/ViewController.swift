993ce9c534a23f0f0d577d9674f37dd6a68c60dc
손명기 <audrl1010@naver.com>
2019-09-19 17:54:00 +0900

    Initial Commit

---
+++ TravisCI_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisCI_Test
+//
+//  Created by myung gi son on 19/09/2019.
+//  Copyright © 2019 Myung Gi Son. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
